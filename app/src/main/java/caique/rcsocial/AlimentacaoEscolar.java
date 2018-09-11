5c7fde6e3da6270c2d92cb32e86cf94f14998c05
Caique Guedes <caique-guedes@hotmail.com>
2018-09-11 18:22:28 -0300

    alterações no código pra funcionalidade da bottom view

--- app/src/main/java/caique/rcsocial/AlimentacaoEscolar.java
+++ app/src/main/java/caique/rcsocial/AlimentacaoEscolar.java
@@ -1,20 +1,41 @@
 package caique.rcsocial;
 
-import android.app.Fragment;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.v4.app.FragmentTransaction;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.Button;
+import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class AlimentacaoEscolar extends AppCompatActivity {
+    private FirebaseAuth mAuth;
+
+    TextView boasVindas;
+
+    FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+    private BottomNavigationView navigationView;
+
+
+    // configurar botões da navbar
+
+    private void setBoasVindas(){
+        String nome = user.getDisplayName();
+        boasVindas = (TextView)findViewById(R.id.tf_boasvindas);
+
+        if(Character.toString(nome.charAt(nome.length() - 1)) == "A"){
+            boasVindas.setText(String.format("Seja bem vinda %s!",nome));
+        }else{
+            boasVindas.setText(String.format("Seja bem vindo %s!",nome));
+        }
+
+    }
+
 
 
 
@@ -23,7 +44,46 @@ public class AlimentacaoEscolar extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_alimentacao_escolar);
 
+        // setar botoes da navibar
+        BottomNavigationView bottomNavi = findViewById(R.id.bott);
+        bottomNavi.setOnNavigationItemSelectedListener(navListener);
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,new PadraoFragment()).commit();
+
+    }
 
 
+    private BottomNavigationView.OnNavigationItemSelectedListener navListener = new BottomNavigationView.OnNavigationItemSelectedListener() {
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            Fragment selectedFragment = null;
+
+            switch (item.getItemId()){
+                case R.id.action_perfil:
+                    selectedFragment = new PerfilFragment();
+                    break;
+                case R.id.action_inicio:
+                    selectedFragment = new PadraoFragment();
+                    break;
+                case R.id.action_auditoria:
+                    selectedFragment = new AuditoriasFragment();
+                    break;
+                case R.id.action_monitoramento:
+                    selectedFragment = new MonitoriasFragment();
+                    break;
+                case R.id.action_sair:
+                    getSupportFragmentManager().popBackStack(null, FragmentManager.POP_BACK_STACK_INCLUSIVE);
+                    Intent sair = new Intent(AlimentacaoEscolar.this, Login.class);
+                    sair.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK |Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                    startActivity(sair);
+                    finish();
+                    break;
+            }
+
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,selectedFragment).commit();
+            return  true;
+        }
+    };
+    public void init(){
+        setBoasVindas();
     }
 }
